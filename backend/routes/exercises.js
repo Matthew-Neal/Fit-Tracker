ef4750e858b6bcd9924b902f1384cd52ad06d982
Matt Neal <impulseimg@gmail.com>
2020-10-13 14:52:31 -0400

    DatePicker

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -48,7 +48,8 @@ router.route('/update/:id').post((req, res) => {
             exercise.save()
                 .then(() => res.json('Exercise updated'))
                 .catch(err => res.status(400).json('Error: + err'))
-        });
-})
+        })
+        .catch(err => res.status(400).json('Error: + err'))
+});
 
 module.exports = router; 
